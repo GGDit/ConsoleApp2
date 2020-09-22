a15de5abfd8a7c3e6d6ffc59592f69d35550d38a
GGDit <gvozdyov.gleb@users.noreply.github.com>
2020-09-22 20:55:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5] { 2, 4, 6, 8, 10 };
+
+            int sum = 0;
+
+            for (int i = 0; i < 5; i++) sum = sum + a[i];
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
